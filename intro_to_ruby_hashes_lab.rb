3af853770a70e42df904894d9cb96292ee41fa13
Kim Lewis <kalewi2@gmail.com>
2019-09-01 18:13:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,10 @@ def monopoly_with_third_tier
    :railroads => {
      :pieces => 4, 
      :rent_in_dollars => {
-       :one=> "one",
-       :two=> "two",
-       :three=> "three",
-       :four=> "four"
+       :one=> 1,
+       :two=> 2,
+       :three=> 3,
+       :four=> 4
    }
  }  
 }
