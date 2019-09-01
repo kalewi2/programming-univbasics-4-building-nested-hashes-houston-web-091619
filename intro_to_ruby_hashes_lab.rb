e035c78854770ae28c63e2fe8de44612420a79ac
Kim Lewis <kalewi2@gmail.com>
2019-09-01 17:44:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- monopoly = {:railroads => {pieces: 4}}
- monopoly[:railroads][
+ monopoly = {:railroads => {pieces: 4, rent_in_dollars:[]}}
+ 
 end
 
 def monopoly_with_fourth_tier
